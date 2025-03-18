997653a2c32093d785f4b3cd64748018dfe23dfb
ShiningLava <crgpwns@gmail.com>
2025-03-17 19:44:04 -0600

    added basi layout for main.py for config, argparser

--- main.py
+++ main.py
@@ -1,2 +1,38 @@
 import random
 import json
+import argparse
+
+VERSION = "1.1.0"
+
+with open('config.json', 'r') as g:
+    config = json.load(g)
+
+sites = 
+pc_endpoints = 
+printers = 
+telephones = 
+cameras = 
+network_budget = 
+
+def main():
+
+def argparser():
+    global sites
+    global pc_endpoints
+    global printers
+    global telephones
+    global cameras
+    global network_budget
+    parser.add_argument("--config", "-c", type=argparse.FileType('r', encoding='UTF-8'))
+    #parser.add_argument("")
+
+    if args.config:
+        try:
+            config = json.load(args.config)
+            parser.set_defaults(**config)
+        except Exception as e:
+            print(e)
+
+    return parser.parse_args()
+if __name__ == "__main__":
+    main()
